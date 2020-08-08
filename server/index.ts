ffff3af0896d391e6742d790e1b8b8c222af7f64
Lucas <lucasvalenteds@gmail.com>
2020-08-08 11:16:35 -0300

    Adding server

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import * as HTTP from "http";
+import Console from "console";
+import Process from "process";
+
+const port = Process.env.PORT;
+
+const server = HTTP.createServer((_, response) => {
+  response.statusCode = 200;
+  response.write(JSON.stringify({ message: "Hello World" }));
+  response.end();
+});
+
+server
+  .listen(port)
+  .once("listening", () => Console.debug("Server running on port %d", port));
